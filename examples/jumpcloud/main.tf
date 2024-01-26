81cbe2ebd73eb04faf54c6bc8eda9323399d92bf
Kevin Gibson <kgibson@spotnana.com>
2024-01-26 11:23:52 -0800

    Logging

--- examples/jumpcloud/main.tf
+++ examples/jumpcloud/main.tf
@@ -16,9 +16,13 @@ provider "snjumpcloud" {
 
 data "snjumpcloud_usergroup" "allusergroups" {}
 
-#output "allusergroups" {
-##  value = data.snjumpcloud_usergroup.allusergroups
-#}
+output "allusergroups" {
+  value = data.snjumpcloud_usergroup.allusergroups
+}
+
+
+// export TF_VAR_api_key=$JC_API_KEY
+
 
 // terraform plan -var "api_key=$JC_API_KEY"
-// api_key=$JC_API_KEY terraform plan
+// api_key=$JC_API_KEY TF_LOG=TRACE terraform plan
